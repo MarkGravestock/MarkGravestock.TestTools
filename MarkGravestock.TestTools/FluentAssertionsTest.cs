4ffe1d154b3ae80ccc93b6dbdc2f3dfbcd80fd6a
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-09-13 12:56:54 +0100

    Initial commit

---
+++ MarkGravestock.TestTools/FluentAssertionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using Xunit;
+
+namespace MarkGravestock.TestTools
+{
+    public class FluentAssertionsTest
+    {
+        [Fact]
+        public void it_can_compare_object_instances()
+        {
+            new Money {Amount = 50, Currency = Money.CurrencyCode.GBP}.Should().NotBeSameAs(new Money {Amount = 50, Currency = Money.CurrencyCode.GBP});
+        }
+        
+        [Fact]
+        public void it_can_compare_object_equality()
+        {
+            new Money {Amount = 50, Currency = Money.CurrencyCode.GBP}.Should().BeEquivalentTo(new Money {Amount = 50, Currency = Money.CurrencyCode.GBP});
+        }
+
+        [Fact]
+        public void it_can_compare_object_equality_when_different()
+        {
+            new Money {Amount = 50, Currency = Money.CurrencyCode.GBP}.Should().NotBeEquivalentTo(new Money {Amount = 50, Currency = Money.CurrencyCode.USD});
+        }
+
+        [Fact]
+        public void calculator_should_perform()
+        {
+            Action calculation = (() => Calculator.Add(2, 2));
+            
+            calculation.ExecutionTime().Should().BeLessThan(200.Milliseconds());
+        }
+    }
+}
---
+++ MarkGravestock.TestTools/XUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MarkGravestock.TestTools
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
